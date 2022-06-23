b78f037ea2b83fb09bc05137afa58a0d7cc6d7e3
ArturoPichardo666 <daniel.pichardo.g@uinenlinea.mx>
2022-06-22 22:10:45 -0500

    first commit

--- EXAPARI.py
+++ EXAPARI.py
@@ -1,16 +1,15 @@
-#pedir al usurio 2 numeros A y B
+print("Dame un numero para A")
+a =int(input())
+b = 0
+while a >= b :
+    print("Dame un numero para B")
+    b =int(input())
+    suma = b + a
+    print("la suma es",suma)
+if a <= b or b <= b:
+    Multiplicacion= b * a
+    print("La Multiplicacion es:",Multiplicacion)
+while b >= a:
+    print(b)
+    b = b - 1
 
-
-
-n=True
-suma=0
-while n:
-    print("Dame el numero 1")
-num1=int (input())
-print("Dame el numero 2")
-num2=int (input())
-if num1 < 0 and num2 > 0 or num2 > 0 and num1 < 0:
-    suma= num1 + num2   
-    print("La suma es:",suma)
-elif num1 < 0 and num2 < 0:
-    multiplicacion = num1 * num2
